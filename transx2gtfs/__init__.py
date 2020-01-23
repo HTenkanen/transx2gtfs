7ad90fcba89ff955dcd13b399c30a31050a9a2c3
Henrikki Tenkanen <henrikki.tenkanen@helsinki.fi>
2020-01-23 12:51:34 +0000

    Convert into a library.

---
+++ transx2gtfs/__init__.py
@@ -0,0 +1,1 @@
+from transx2gtfs.transx2gtfs import convert
---
+++ transx2gtfs/__main__.py
@@ -0,0 +1,3 @@
+
+#data_dir = r"C:\HY-DATA\hentenka\Data\TFL\LULDLRTRAMRIVERCABLE 26022019"
+#output_fp = r"C:\HY-DATA\hentenka\Data\TFL\London_metro_GTFS.zip"
