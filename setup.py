97ac3a9834a67b86a978875b0d89a2067c39efde
Henrikki Tenkanen <henrikki.tenkanen@helsinki.fi>
2020-01-25 13:23:12 +0000

    Bump version.

--- setup.py
+++ setup.py
@@ -30,7 +30,7 @@ with open(path.join(this_directory, 'README.md'), encoding='utf-8') as f:
 
 setup(
     name='transx2gtfs',
-    version='0.3.1',
+    version='0.3.2',
     license='MIT',
     description='A Python tool to convert TransXchange data into GTFS.',
     long_description=long_description,
